e6b3fdb24e1110628c9ceb4dd796f9e3c5c7f96b
UKLsolong <71876139+UKLsolong@users.noreply.github.com>
2020-10-07 20:20:04 +0800

    Add files via upload

---
+++ 未命名4.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{	
+	int age = 20;
+	int weght = 30;
+	char ch ='nb';
+	char cx ='n'; 
+	char cl ='b';
+	char cj ='Å£'; //×Ö·û²»ÄÜÊÇ ºº×Ö£¿ 
+	printf("hello word\n");
+	printf("%d\n%d\n%c\n%c\n%c\n%c\n",age,weght,ch,cj,cx,cl);
+	return 0;
+}
